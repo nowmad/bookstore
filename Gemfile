3228009802ebdc0d661826438df08295c0de9f82
Fabiano Morais <fmorais@gmail.com>
2015-05-31 21:54:57 +1000

    Add figaro gem

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.4'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'paperclip', '~> 4.2'
+gem "paperclip-dropbox", ">= 1.1.7"
+gem "figaro"
 
 group :production do
 	gem 'pg'
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -65,6 +65,7 @@ class ListingsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_listing
       @listing = Listing.find(params[:id])
+
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
